4b6f12fd2061760d7c8d7aa722de5d0bbb5bec69
PreethiAppionx <146828314+PreethiAppionx@users.noreply.github.com>
2023-10-17 13:30:42 +0530

    outer strapi folder

---
+++ src/api/getsratrted/routes/getsratrted.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * getsratrted router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::getsratrted.getsratrted');
